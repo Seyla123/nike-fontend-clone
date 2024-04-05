49dc3c866e627fb0629af1ab89aff565e887bbcc
Seyla123 <Mrrseyla.758@gmail.com>
2024-04-05 19:38:59 +0700

    new set up resauble layout

--- Src/scripts/index.js
+++ Src/scripts/index.js
@@ -1,7 +1,28 @@
-const nav = document.querySelector('.navbar')
-fetch('./components/layouts/navbar.html')
+// Header
+const header = document.querySelector('.header-layout')
+fetch('../components/layouts/header.html')
+.then(res=>res.text())
+.then(data=>{
+    header.innerHTML=data
+})
+.catch(error => console.error('Error fetching included file:', error));
+////////
+
+//Navbar
+const nav = document.querySelector('.navbar-layout')
+fetch('../components/layouts/navbar.html')
 .then(res=>res.text())
 .then(data=>{
     nav.innerHTML=data
 })
 .catch(error => console.error('Error fetching included file:', error));
+//Hero Section 
+
+const hero_section = document.querySelector('.hero-section-layout')
+fetch('../components/layouts/hero-section.html')
+.then(res=>res.text())
+.then(data=>{
+    hero_section.innerHTML=data
+})
+.catch(error => console.error('Error fetching included file:', error));
+//////////
